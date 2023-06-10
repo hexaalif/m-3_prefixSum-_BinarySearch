c59e58ff5611f37c212f83fa5c300263a63be494
hexaalif <hexaalif2020@gmail.com>
2023-06-10 12:56:21 +0600

    files

--- Sorted.cpp
+++ Sorted.cpp
@@ -4,6 +4,37 @@ using namespace std;
 
 int main()
 {
+    int t;
+    cin >> t;
+    bool sorted = true;
+    for (int i = 1; i <= t; i++)
+    {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int j = 0; j < n; j++)
+        {
+            cin >> arr[j];
+        }
+        for (int j = 0; j < n - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                sorted = false;
+            }
+            // cout<<arr[i]<<" ";
+        }
+
+        if (sorted)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+        // cout << endl;
+    }
 
     return 0;
 }
